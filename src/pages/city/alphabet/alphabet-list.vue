67c6556e5cbba0e4854c90026159fe746da04c74
xiaotee <chasekafei@gmail.com>
2018-06-02 23:50:38 +0800

    feat: 增加城市列表组件

---
+++ src/pages/city/alphabet/alphabet-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="alphabet-list-wrap">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlphabetList',
+  data() {
+    return {
+      test: 233
+    }
+  },
+  methods: {
+    // 实现功能
+    // 解析父级传入的城市数据json；根据父级传入的字母跳转到相应位置；根据父级传入的文本筛选列表；
+    test() {
+      console.log(233);
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
