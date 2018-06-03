d5b903c7efab7590ca54e048d4b9b88bcf41c406
xiaotee <chasekafei@gmail.com>
2018-06-03 15:39:53 +0800

    fix: 全局样式修改

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,25 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+html {
+  line-height: 1.4;
+  font-size: 625%;
+}
+
+body {
+  margin: 0;
+}
+
+body,
+input,
+textarea,
+select {
+  font-size: 0.14rem;
+  color: #333;
+  font-family: "PingFangSC", "Microsoft YaHei", "微软雅黑", SimSun, "宋体", Heiti, "黑体", sans-serif;
 }
 </style>
