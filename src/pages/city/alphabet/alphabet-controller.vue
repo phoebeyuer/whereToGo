a0ec0a1fd6709b13a69fc58108ca0505a8b8d8f3
xiaotee <chasekafei@gmail.com>
2018-06-04 17:03:44 +0800

    feat: 完成 alphabet 边栏基本样式

---
+++ src/pages/city/alphabet/alphabet-controller.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="alphabet-controller-component" ref="controllerWrap">
+    <div class="letter-wrap">
+      <ul class="letter-list">
+        <li class="letter-item" v-for="letter in letterList" :key="letter">
+          <span class="letter-name">{{ letter }}</span>
+          <span class="letter-bubble">{{ letter }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlphabetController',
+  props: ['letterList'],
+  data() {
+    return {
+      test: 233
+    }
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    // 实现功能
+    // 获取字母列表；点击字母向父级传数据；在字母上滑动向父级传输数据；在字母上滑动有悬浮气泡提示；
+
+  }
+}
+</script>
+
+<style lang="less">
+  .alphabet-controller-component{
+    width: 100%;
+    .letter-wrap{
+
+    }
+    .letter-list{
+      display: flex;
+      flex-direction: column;
+      list-style: none;
+      padding: 0;
+      .letter-item{
+        flex: 1;
+        position: relative;
+        cursor: pointer;
+        text-align: center;
+
+        .letter-name{
+          color: #00BCD4;
+          width: 100%;
+          padding: 0.02rem;
+          text-align: center;
+        }
+        .letter-bubble{
+          @bubbleSize: 0.36rem;
+          @bubbleColor: #00BCD4;
+
+          position: absolute;
+          top: 50%;
+          left: 0;
+          background-color: #00BCD4;
+          border-radius: 50%;
+          color: #fff;
+          display: block;
+          line-height: @bubbleSize;
+          height: @bubbleSize;
+          width: @bubbleSize;
+          text-align: center;
+          transform: translate(-100%, -50%);
+          opacity: 0;
+          transition: opacity 0.3s;
+          &::after{
+            content: ' ';
+            border: @bubbleSize/2*0.707 solid @bubbleColor;
+            border-color: transparent transparent transparent @bubbleColor;
+            position: absolute;
+            top: 50%;
+            right: @bubbleSize/2*0.707/2*0.8;
+            transform: translate(100%, -50%);
+          }
+        }
+        &:hover .letter-bubble{
+          opacity: 1;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,18 +2,29 @@
   <div class="city-wrap">
     test
     <div class="alphabet-list-wrap">
-      <alphabet-list filterStr="" letter="B"></alphabet-list>
+      <alphabet-list filterStr="" :letter="currentLetter"></alphabet-list>
+    </div>
+    <div class="alphabet-controller-wrap">
+      <alphabet-controller :letterList="letterList"></alphabet-controller>
     </div>
   </div>
 </template>
 
 <script>
 import AlphabetList from './alphabet/alphabet-list'
+import AlphabetController from './alphabet/alphabet-controller'
 
 export default {
   name: 'City',
   components: {
-    'alphabet-list': AlphabetList
+    'alphabet-list': AlphabetList,
+    'alphabet-controller': AlphabetController
+  },
+  data() {
+    return {
+      currentLetter: 'B',
+      letterList: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']
+    }
   }
 }
 </script>
@@ -22,5 +33,14 @@ export default {
 .alphabet-list-wrap{
   height: 300px;
   position: relative;
+}
+.alphabet-controller-wrap{
+  position: fixed;
+  top: 0;
+  right: 0rem;
+  display:flex;
+  align-items:center;
+  height: 100vh;
+  width: 10%;
 }
 </style>
