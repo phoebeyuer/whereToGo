6d9a0bab3decf46d1f4621ead9ae42e452ef9993
shujiawei <1078921774@qq.com>
2018-06-10 23:01:57 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,5 @@
 </script>
 
 <style>
-*{margin: 0;padding: 0;}
-  html {
-    line-height: 1.4;
-    font-size: 625%;
-    height: 100%;
-    width: 100%;
-  }
-  body,
-  input,
-  textarea,
-  select {
-    font-size: 0.14rem;
-    color: #333;
-    font-family: "PingFangSC", "Microsoft YaHei", "微软雅黑", SimSun, "宋体", Heiti, "黑体", sans-serif;
-  }
+
 </style>
--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,6 +2,12 @@
 	<div class="city">
 		<cityHeader :cities="cities"></cityHeader>
 		<city-position></city-position>
+		<div class="alphabet-list-wrap">
+			<alphabet-list filterStr="" :letter="currentLetter"></alphabet-list>
+		</div>
+		<div class="alphabet-controller-wrap">
+			<alphabet-controller :letterList="letterList" :letter.sync="currentLetter"></alphabet-controller>
+		</div>
 <!-- 	<citySearch></citySearch>
 		<cityList></cityList>
 		<cityAlphabet></cityAlphabet> -->
@@ -12,14 +18,20 @@
 	import axios from 'axios'
 	import cityHeader from "./components/header"
 	import cityPosition from "./components/position"
+	import AlphabetList from './alphabet/alphabet-list'
+	import AlphabetController from './alphabet/alphabet-controller'
 	export default{
 		components: {
 			cityHeader,
-			cityPosition
+			cityPosition,
+			AlphabetList,
+			AlphabetController
 		},
 		data(){
 			return{
-				cities: {}
+				cities: {},
+				currentLetter: 'B',
+      			letterList: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']
 			}
 		},
 		mounted(){
@@ -40,4 +52,18 @@
 	}
 </script>
 
-<style scoped></style>
+<style scoped>
+	.alphabet-list-wrap{
+		height: 300px;
+		position: relative;
+	}
+	.alphabet-controller-wrap{
+		position: fixed;
+		top: 0;
+		right: 0rem;
+		display:flex;
+		align-items:center;
+		height: 100vh;
+		width: 10%;
+	}
+</style>
