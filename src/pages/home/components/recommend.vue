10cacf492505f223f044d54f378fc60e3a0c7879
largeMing <aming_zz@163.com>
2018-06-22 08:58:29 +0800

    修改首页样式

--- src/pages/home/components/recommend.vue
+++ src/pages/home/components/recommend.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="recomend">
+  <div>
     <p class="title">热销推荐</p>
     <ul>
-      <li v-for="item of recommed" class="left">
-        <img :src="item.imgUrl"/>
-        <div class="right">
-          <p>{{item.title}}</p>
-          <p class="huise">{{item.desc}}</p>
-          <router-link tag='button' :to="'/detail/'+item.id" :key="item.id">查看详情</router-link>
+      <router-link
+        tag="li"
+        v-for="item of recommed"
+        class="item"
+        :key="item.id"
+        :to="'/detail/'+item.id"
+      >
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-info">
+          <div class="item-title">{{item.title}}</div>
+          <div class="item-desc">{{item.desc}}</div>
+          <router-link class="item-button" tag='button' :to="'/detail/'+item.id" :key="item.id">查看详情</router-link>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
@@ -23,45 +29,41 @@
   }
 </script>
 
-<style scoped="scoped">
-  .title {
-    font-weight: bold;
-  }
+<style scoped lang="stylus" type="text/stylus">
+  .title
+    text-indent .2rem
+    line-height .8rem
+    margin-top .2rem
+    background #eee
 
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  ul li {
-    text-align: left;
-  }
-
-  .huise {
-    color: #999;
-  }
-
-  .left img {
-    width: 20%;
-  }
-
-  .right {
-    float: right;
-    width: 78%;
-  }
-
-  p {
-    padding: 10px;
-    margin: 0;
-  }
-
-  button {
-    border: 0;
-    color: white;
-    padding: 5px 10px;
-    border-radius: 5px;
-    background: #FF9300;
-  }
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color: #ccc
+        overflow hidden
+        white-space nowrap
+        text-overflow ellipsis
+      .item-button
+        background #ff9300
+        border-radius .06rem
+        color: #fff
+        margin-top .16rem
+        line-height .44rem
+        padding 0 .2rem
 
 </style>
